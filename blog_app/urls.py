be1f551ca26b71d027bbc71306a673eff2ef9aa7
Sujon Roy <104683977+Speed-No-1@users.noreply.github.com>
2022-09-03 12:04:33 +0600

    no change

---
+++ blog_app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from blog_app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+]
